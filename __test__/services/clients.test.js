31086f88b6961039f9e5c9c27fc6139ae97c4186
Einar Persson <me@einarpersson.com>
2018-12-10 14:14:10 +0100

    Add client registration through api

---
+++ __test__/services/clients.test.js
@@ -0,0 +1,68 @@
+const { create, get } = require('../../lib/services/clients')
+const postgres = require('../../lib/adapters/postgres')
+jest.mock('../../lib/adapters/postgres')
+
+describe('services/clients', () => {
+  let connection
+
+  beforeEach(() => {
+    connection = {
+      query: jest.fn(),
+      end: jest.fn()
+    }
+    postgres.connect.mockResolvedValue(connection)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('#create', () => {
+    let data
+
+    beforeEach(() => {
+      data = {
+        clientId: 'mycv.example',
+        name: 'mycv',
+        description: 'this is the best app there is',
+        jwksUrl: '/jwks'
+      }
+
+      connection.query.mockResolvedValue({
+        rowCount: 1
+      })
+    })
+
+    it('calls connect once', async () => {
+      await create(data)
+
+      expect(postgres.connect).toHaveBeenCalledTimes(1)
+    })
+
+    it('throws if number of rows affected is not 1', async () => {
+      connection.query.mockResolvedValue(5)
+
+      await expect(create(data)).rejects.toThrow()
+    })
+  })
+
+  describe('#get', () => {
+    beforeEach(() => {
+      connection.query.mockResolvedValue({
+        rows: [{
+          client_id: 'mycv.example',
+          display_name: 'mycv',
+          description: 'this is the best app there is',
+          jwks_url: '/jwks',
+          public_key: 'asdsadads'
+        }]
+      })
+    })
+
+    it('calls connect once', async () => {
+      await get('mycv.example')
+
+      expect(postgres.connect).toHaveBeenCalledTimes(1)
+    })
+  })
+})
