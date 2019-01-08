8dac90e2ba8e26f595378207d8830ebb6907a5be
Johan Öbrink <johan.obrink@gmail.com>
2019-01-08 14:02:41 +0100

    Validates unsafe flag for signatures

--- __test__/middleware/auth.test.js
+++ __test__/middleware/auth.test.js
@@ -31,13 +31,15 @@ describe('/middleware/auth', () => {
     let payload
     describe('with clientId and kid', () => {
       beforeEach(() => {
-        app.post('/', signed({ unsafe: true }), route)
+        app.post('/unsafe', signed({ unsafe: true }), route)
+        app.post('/safe', signed({ }), route)
         app.use((err, req, res, next) => res.status(err.status).send(err))
         api = createApi(app)
 
         payload = {
           data: {
             clientId: 'mydata.work',
+            unsafe: true,
             foo: 'bar'
           },
           signature: {
@@ -48,62 +50,69 @@ describe('/middleware/auth', () => {
         payload.signature.data = sign(payload.data, clientKey.privateKey)
       })
       it('gives a validation error if data and signature are not present', async () => {
-        const res = await api.post('/', {})
+        const res = await api.post('/unsafe', {})
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if no data is present', async () => {
         payload.data = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if no signature is present', async () => {
         payload.signature = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if data.clientId is missing', async () => {
         payload.data.clientId = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if signature.kid is missing', async () => {
         payload.signature.kid = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if signature.data is missing', async () => {
         payload.signature.data = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
+      it('throws 403 if unsafe is true but not allowed', async () => {
+        clientsService.get.mockResolvedValue()
+        const res = await api.post('/safe', payload)
+        expect(route).not.toBeCalled()
+        expect(res.status).toEqual(403)
+        expect(res.body.message).toEqual('Unsafe host not allowed')
+      })
       it('throws 401 if kid is client_key and clientId cannot be found', async () => {
         clientsService.get.mockResolvedValue()
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(401)
         expect(res.body.message).toEqual('Unknown clientId')
       })
       it('throws 403 if signature cannot be validated', async () => {
         payload.signature.data = 'bork'
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(403)
         expect(res.body.message).toEqual('Invalid signature')
       })
       it('calls route with data part of payload if signature is verified', async () => {
-        await api.post('/', payload)
+        await api.post('/unsafe', payload)
         expect(route).toHaveBeenCalled()
         const [[req]] = route.mock.calls
         expect(req.body).toEqual(payload.data)
@@ -132,12 +141,12 @@ describe('/middleware/auth', () => {
         })
         it('throws a 401 if specified key cannot be retrieved', async () => {
           cv.jwksUrl = '/wrong/url'
-          const res = await api.post('/', payload)
+          const res = await api.post('/unsafe', payload)
           expect(res.status).toEqual(401)
           expect(res.body.message).toEqual('Could not retrieve key')
         })
         it('calls route with data part of payload if signature is verified', async () => {
-          const res = await api.post('/', payload)
+          const res = await api.post('/unsafe', payload)
           expect(res.status).toEqual(200)
           expect(route).toHaveBeenCalled()
           const [[req]] = route.mock.calls
@@ -147,7 +156,7 @@ describe('/middleware/auth', () => {
     })
     describe('with publicKey', () => {
       beforeEach(async () => {
-        app.post('/', signed({ unsafe: true, withKey: true }), route)
+        app.post('/unsafe', signed({ unsafe: true, withKey: true }), route)
         app.use((err, req, res, next) => res.status(err.status).send(err))
         api = createApi(app)
 
@@ -163,48 +172,48 @@ describe('/middleware/auth', () => {
         payload.signature.data = sign(payload.data, clientKey.privateKey)
       })
       it('gives a validation error if data and signature are not present', async () => {
-        const res = await api.post('/', {})
+        const res = await api.post('/unsafe', {})
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if no data is present', async () => {
         payload.data = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if no signature is present', async () => {
         payload.signature = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if data.publicKey is missing', async () => {
         payload.data.publicKey = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 400 if signature.data is missing', async () => {
         payload.signature.data = undefined
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(400)
         expect(res.body.name).toEqual('ValidationError')
       })
       it('throws 403 if signature cannot be validated', async () => {
         payload.signature.data = 'bork'
-        const res = await api.post('/', payload)
+        const res = await api.post('/unsafe', payload)
         expect(route).not.toBeCalled()
         expect(res.status).toEqual(403)
         expect(res.body.message).toEqual('Invalid signature')
       })
       it('calls route with data part of payload if signature is verified', async () => {
-        await api.post('/', payload)
+        await api.post('/unsafe', payload)
         expect(route).toHaveBeenCalled()
         const [[req]] = route.mock.calls
         expect(req.body).toEqual(payload.data)
