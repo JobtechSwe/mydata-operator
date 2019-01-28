555557aa350f753636e699651f5954fb9d633e3c
Johan Öbrink <johan.obrink@gmail.com>
2019-01-28 13:25:22 +0100

    Consent results are saved to db

--- __test__/services/consents/create.test.js
+++ __test__/services/consents/create.test.js
@@ -19,6 +19,7 @@ describe('services/consents #create', () => {
   beforeEach(() => {
     consentBody = {
       consentRequestId: '809eea87-6182-4cb4-8d6e-df6d411149a2',
+      consentEncryptionKeyId: 'http://localhost:4000/jwks/enc_20190115082310',
       consentEncryptionKey: base64('-----BEGIN RSA PUBLIC KEY----- consent'),
       accountId: 'b60c5a93-ed93-41bd-8f77-176c564fb976',
       accountKey: base64('-----BEGIN RSA PUBLIC KEY----- account'),
@@ -30,7 +31,7 @@ describe('services/consents #create', () => {
           area: 'education',
           description: 'Stuff',
           lawfulBasis: 'strength',
-          permissions: ['read'],
+          permissions: ['READ'],
           purpose: 'dominance'
         }
       ]
@@ -41,9 +42,15 @@ describe('services/consents #create', () => {
   })
   it('inserts consent into db', async () => {
     await create(consentBody)
-    expect(postgres.client.query).toHaveBeenCalledTimes(1)
-    expect(postgres.client.query).toBeCalledWith(
-      expect.any(String),
+
+    let calls = 1
+
+    // Transaction begin
+    expect(postgres.client.query).toHaveBeenNthCalledWith(calls++, 'BEGIN')
+
+    // Consent request
+    expect(postgres.client.query).toHaveBeenNthCalledWith(calls++,
+      expect.stringMatching(/^INSERT INTO consent_requests/),
       [
         consentBody.consentRequestId,
         expect.any(String), // consentId
@@ -51,6 +58,41 @@ describe('services/consents #create', () => {
         consentBody.clientId,
         JSON.stringify(consentBody)
       ])
+    const consentId = postgres.client.query.mock.calls[1][1][1]
+
+    // Scope
+    expect(postgres.client.query).toHaveBeenNthCalledWith(calls++,
+      expect.stringMatching(/^INSERT INTO scope/),
+      [
+        expect.any(String), // scope id
+        consentId, // consentId
+        'http://cv.com',
+        'education',
+        'Stuff',
+        'dominance',
+        'strength',
+        true,
+        false
+      ])
+    const scopeId = postgres.client.query.mock.calls[2][1][0]
+
+    // Encryption keys
+    expect(postgres.client.query).toHaveBeenNthCalledWith(calls++,
+      expect.stringMatching(/^INSERT INTO encryption_keys/),
+      [
+        'http://localhost:4000/jwks/enc_20190115082310',
+        '-----BEGIN RSA PUBLIC KEY----- consent'
+      ])
+    expect(postgres.client.query).toHaveBeenNthCalledWith(calls++,
+      expect.stringMatching(/^INSERT INTO scope_keys/),
+      [
+        scopeId,
+        'http://localhost:4000/jwks/enc_20190115082310'
+      ])
+
+    // Transaction commit
+    expect(postgres.client.query).toHaveBeenNthCalledWith(calls, 'COMMIT')
+    expect(postgres.client.query).toHaveBeenCalledTimes(calls)
     expect(postgres.client.end).toBeCalledTimes(1)
   })
   it('posts the right stuff to the client', async () => {
@@ -68,7 +110,7 @@ describe('services/consents #create', () => {
             area: 'education',
             description: 'Stuff',
             lawfulBasis: 'strength',
-            permissions: ['read'],
+            permissions: ['READ'],
             purpose: 'dominance'
           }
         ],
