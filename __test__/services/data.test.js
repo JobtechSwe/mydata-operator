34234edffa1a71009b665d1e75b0b2070ebb9adc
Johan Öbrink <johan.obrink@gmail.com>
2019-02-01 14:23:01 +0100

    Write to PDS

--- __test__/services/data.test.js
+++ __test__/services/data.test.js
@@ -56,56 +56,72 @@ describe('services/data', () => {
   afterEach(() => {
     pg.clearMocks()
   })
-  it('retrieves consent information from db', async () => {
-    await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv')
-    expect(pg.client.query).toHaveBeenCalledTimes(1)
-  })
-  it('filters on consentId', async () => {
-    await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8')
-    expect(pg.client.query).toHaveBeenCalledWith(
-      expect.stringMatching(/SELECT([\s\S]*)WHERE([\s\S]*)consent_id = \$1/gm),
-      ['b106b599-d821-48cb-b588-e583d6dc41e8']
-    )
-  })
-  it('filters on consentId and domain', async () => {
-    await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost')
-    expect(pg.client.query).toHaveBeenCalledWith(
-      expect.stringMatching(/SELECT([\s\S]*)WHERE([\s\S]*)consent_id = \$1([\s\S]*)AND([\s\S]*)domain = \$2/gm),
-      ['b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost']
-    )
-  })
-  it('filters on consentId, domain and area', async () => {
-    await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv')
-    expect(pg.client.query).toHaveBeenCalledWith(
-      expect.stringMatching(/SELECT([\s\S]*)WHERE([\s\S]*)consent_id = \$1([\s\S]*)AND([\s\S]*)domain = \$2([\s\S]*)AND([\s\S]*)area = \$3/gm),
-      ['b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv']
-    )
-  })
-  it('reads and returns data for all consents', async () => {
-    const expected = {
-      'localhost:4000': {
-        'cv': '{"cv":"foo"}',
-        'personal': '{"name":"Johan"}'
-      },
-      'linkedin.com': {
-        'experience': '{"experience":1}'
+  describe('#read', () => {
+    it('retrieves consent information from db', async () => {
+      await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv')
+      expect(pg.client.query).toHaveBeenCalledTimes(1)
+    })
+    it('filters on consentId', async () => {
+      await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8')
+      expect(pg.client.query).toHaveBeenCalledWith(
+        expect.stringMatching(/SELECT([\s\S]*)WHERE([\s\S]*)consent_id = \$1/gm),
+        ['b106b599-d821-48cb-b588-e583d6dc41e8']
+      )
+    })
+    it('filters on consentId and domain', async () => {
+      await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost')
+      expect(pg.client.query).toHaveBeenCalledWith(
+        expect.stringMatching(/SELECT([\s\S]*)WHERE([\s\S]*)consent_id = \$1([\s\S]*)AND([\s\S]*)domain = \$2/gm),
+        ['b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost']
+      )
+    })
+    it('filters on consentId, domain and area', async () => {
+      await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv')
+      expect(pg.client.query).toHaveBeenCalledWith(
+        expect.stringMatching(/SELECT([\s\S]*)WHERE([\s\S]*)consent_id = \$1([\s\S]*)AND([\s\S]*)domain = \$2([\s\S]*)AND([\s\S]*)area = \$3/gm),
+        ['b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv']
+      )
+    })
+    it('reads and returns data for all consents', async () => {
+      const expected = {
+        'localhost:4000': {
+          'cv': '{"cv":"foo"}',
+          'personal': '{"name":"Johan"}'
+        },
+        'linkedin.com': {
+          'experience': '{"experience":1}'
+        }
       }
-    }
-    const result = await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8')
-    expect(result).toEqual(expected)
-  })
-  it('returns null for missing data', async () => {
-    dfs.filesystem.data[encodeURIComponent('localhost:4000')] = undefined
-    const expected = {
-      'localhost:4000': {
-        'cv': null,
-        'personal': null
-      },
-      'linkedin.com': {
-        'experience': '{"experience":1}'
+      const result = await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8')
+      expect(result).toEqual(expected)
+    })
+    it('returns null for missing data', async () => {
+      dfs.filesystem.data[encodeURIComponent('localhost:4000')] = undefined
+      const expected = {
+        'localhost:4000': {
+          'cv': null,
+          'personal': null
+        },
+        'linkedin.com': {
+          'experience': '{"experience":1}'
+        }
       }
-    }
-    const result = await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8')
-    expect(result).toEqual(expected)
+      const result = await dataService.read('b106b599-d821-48cb-b588-e583d6dc41e8')
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('#write', () => {
+    it('retrieves consent information from db', async () => {
+      await dataService.write('b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv')
+      expect(pg.client.query).toHaveBeenCalledTimes(1)
+    })
+    it('filters on consentId, domain and area', async () => {
+      await dataService.write('b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv')
+      expect(pg.client.query).toHaveBeenCalledWith(
+        expect.stringMatching(/SELECT([\s\S]*)WHERE([\s\S]*)consent_id = \$1([\s\S]*)AND([\s\S]*)domain = \$2([\s\S]*)AND([\s\S]*)area = \$3/gm),
+        ['b106b599-d821-48cb-b588-e583d6dc41e8', 'localhost', 'cv']
+      )
+    })
   })
 })
